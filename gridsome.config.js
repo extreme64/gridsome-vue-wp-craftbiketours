36f3076838d4cef0970f0542a459183d15c14c49
Mastermind <adam.gicevic@gmail.com>
2020-10-02 02:34:36 +0200

    www

--- gridsome.config.js
+++ gridsome.config.js
@@ -12,7 +12,7 @@ module.exports = {
     {
       use: '@gridsome/source-wordpress',
       options: {
-        baseUrl: process.env.WORDPRESS_URL, // required
+        baseUrl: 'https://forwardcreating.com/', // required
         typeName: 'WordPress', // GraphQL schema name (Optional)
       }
     }
